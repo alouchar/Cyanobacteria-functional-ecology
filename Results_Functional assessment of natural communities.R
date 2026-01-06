3a2434d49589abdd723d0c32d13a5b0f6919d7ee
Arnaud P. Louchart <133276558+alouchar@users.noreply.github.com>
2026-01-06 17:34:34 +0100

    Results_Functional assessment of natural communities.R

--- Results_Functional assessment of natural communities.R
+++ Results_Functional assessment of natural communities.R
@@ -12,109 +12,210 @@
 require(ggnewscale)
 require(ggpmisc)
 
-## 6. Formating functional fingerprints
-# 6.1. Reload the functional responses of Microcystis obtained at the individual level
-dat <- readxl::read_excel("IndCyano_Louchart_Limitation_experiment_June2025.xlsx", sheet = "IndTraits")
-
-# 6.2. Compute the average functional responses of Microcystis
-Ref_transform_average <- 
-  dat %>%
-  group_by(Treatment) %>%
-  dplyr::summarise(across(c('Cell size', Granularity,'Phycocyanin', 'Chlorophyll-a', 'Gas vesicle','PC per size unit', 'Chla per size unit', 'PC:Chla ratio'), mean))
-
-# 6.3. Log10 transformation and normalisation
-Ref_transform_average[,c(2:9)] <- log10(Ref_transform_average[,c(2:9)])
-Ref_transform_average[,c(2:9)] <- scale(Ref_transform_average[,c(2:9)], center = TRUE, scale = TRUE)
-
-## 7. Functional assessment of natural communities
-CommTraits <- readxl::read_excel("IndCyano_Louchart_Limitation_experiment_June2025.xlsx", sheet = "CommTraits")
-CommTraits$Date <- as.Date(CommTraits$Date, format = "%d-%m-%Y")
-
-# 7.1. Log10 transformation and normalisation
-CommTraits[,c(4:11)] <- log10(CommTraits[,c(4:11)])
-CommTraits <- CommTraits[complete.cases(CommTraits),]
-CommTraits[,c(4:11)] <- scale(CommTraits[,c(4:11)], center = TRUE, scale = TRUE)
-
-# 7.2. Plots to produce figure 5
-p1 <- 
-  ggplot(data = CommTraits,aes(x = Date, y = Phycocyanin), shape = 16) +
-  geom_rect(aes(xmin = min(CommTraits$Date), xmax = max(CommTraits$Date), ymin = -2.5, ymax = Ref_transform_average$Phycocyanin[5]), 
-            fill =  "#FF7F0EFF", alpha = 0.01) + # Rectangle for "likely N limitation"
-  geom_rect(aes(xmin = min(CommTraits$Date), xmax = max(CommTraits$Date), ymin = Ref_transform_average$Phycocyanin[5], ymax = 2.5), 
-            fill = "#1F77B4FF", alpha = 0.01) + 
-  annotate("text", x = as.Date("2023-05-15"), y = -2.25, 
-           label = "Likely\nnitrogen limited", color = "black", size = 3) +
-  annotate("text", x = as.Date("2023-05-15"), y = 2.25, 
-           label = "Likely\nlight limited", color = "black", size = 3) +
-  geom_point( ) +
-  geom_line() +
-  geom_point(data = subset(CommTraits, `N Limitation` == "Y"), aes(x = Date, y = Phycocyanin), col = "orange", size = 3, shape = 21,  stroke = 1.2) +
-  scale_x_date(labels = date_format("%b"), date_breaks = "1 month") +
-  geom_hline(yintercept = Ref_transform_average$Phycocyanin[5], linetype = "dotted") +
-  stat_smooth(mapping = aes(y = Phycocyanin, x = Date), method = "lm", se = FALSE, col = "grey20", linetype = "dashed") +
-  # stat_poly_eq(mapping = use_label(c("adj.R2", "P")), size = 4, label.x = 0.95) +
-  ylim(-2.5,2.5) +
-  theme_bw() +
-  xlab("Date") +
-  ylab("Phycocyanin fluorescence (a.u.)") +
-  theme(
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 14)
+## 1. Load the datasets
+dat <-
+  get_dataframe_by_name(
+    filename  = "IndCyano_Louchart_Limitation_experiment_June2025.xlsx",
+    dataset   = "10.34894/9X9YMO",
+    server = "dataverse.nl",
+    .f = function(file) read_excel(file, sheet = "CommTraits"),
   )
-p1
 
-p2 <-  
-  ggplot(data = CommTraits, aes(x = Date, y = `Chlorophyll-a`), shape = 16) +
-  geom_rect(aes(xmin = min(CommTraits$Date), xmax = max(CommTraits$Date), ymin = -2.5, ymax = Ref_transform_average$`Chlorophyll-a`[5]), 
-            fill =  "#FF7F0EFF", alpha = 0.01) +  # Rectangle for "likely N limitation"
-  geom_rect(aes(xmin = min(CommTraits$Date), xmax = max(CommTraits$Date), ymin = Ref_transform_average$`Chlorophyll-a`[5], ymax = 2.5), 
-            fill = "#1F77B4FF", alpha = 0.01) + 
-  annotate("text", x = as.Date("2023-05-15"), y = -2.25, 
-           label = "Likely\nnitrogen limited", color = "black", size = 3) +
-  annotate("text", x = as.Date("2023-05-15"), y = 2.25, 
-           label = "Likely\nlight limited", color = "black", size = 3) +
-  geom_hline(yintercept = Ref_transform_average$`Chlorophyll-a`[5], linetype = "dotted") +
-  geom_point() +
-  geom_line() +
-  geom_point(data = subset(CommTraits, `N Limitation` == "Y"), aes(x = Date, y = `Chlorophyll-a`), col = "orange", size = 3, shape = 21,  stroke = 1.2) +
-  scale_x_date(labels = date_format("%b"), date_breaks = "1 month") +
-  stat_smooth(mapping = aes(y = `Chlorophyll-a`, x = Date), method = "lm", se = FALSE, col = "grey20", linetype = "dashed") +
-  # stat_poly_eq(mapping = use_label(c("adj.R2", "P")), size = 4, label.x = 0.95) +
-  ylim(-2.5,2.5) +
-  theme_bw() +
-  xlab("Date") +
-  ylab("Chlorophyll a fluorescence (a.u.)") +
-  theme(
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 14)
+df <-
+  get_dataframe_by_name(
+    filename  = "IndCyano_Louchart_Limitation_experiment_June2025.xlsx",
+    dataset   = "10.34894/9X9YMO",
+    server = "dataverse.nl",
+    .f = function(file) read_excel(file, sheet = "IndTraits"),
   )
-p2
+df$sample <- NA
 
-p3 <- ggplot() +
-  geom_point(data = CommTraits, mapping = aes(`Chlorophyll-a`, Phycocyanin , col = Date), shape = 16) +
-  geom_point(data = subset(CommTraits, `N Limitation` == "Y"), mapping = aes(`Chlorophyll-a`, Phycocyanin), col = "orange", size = 3, shape = 21,  stroke = 1.2) +
-  scale_color_gradient(low = "grey90", high = "grey10", name = "Date", breaks = as.numeric(CommTraits$Date[seq(1, nrow(CommTraits), length.out = 4)]),
-                       labels = c("12-Apr.","mid June","mid Aug.","25-Oct.")
-  ) +
+env <-
+  get_dataframe_by_name(
+    filename  = "IndCyano_Louchart_Limitation_experiment_June2025.xlsx",
+    dataset   = "10.34894/9X9YMO",
+    server = "dataverse.nl",
+    .f = function(file) read_excel(file, sheet = "EnvData"),
+  )
+env$Date <- as.Date(env$Date, format = "%d-%m-%Y")
+env <- env[,c(1:8,10,9)]
+
+
+## 2. Data preparation - Fig. S9
+# Log10 transformation
+dat[,c(1:8)] <- log10(dat[,c(1:8)])
+
+# Z-score normalisation
+dat[,c(1:8)] <- scale(dat[,c(1:8)], center = TRUE, scale = TRUE)
+
+# Log10 transformation
+df[,c(3:10)] <- log10(df[,c(3:10)])
+
+# Z-score normalisation
+df[,c(3:10)] <- scale(df[,c(3:10)], center = TRUE, scale = TRUE)
+
+# Merge biotic and abiotic data
+dat <- merge(dat, env, by.x = "sample", by.y = "Sample", all.x = TRUE, all.y = TRUE)
+
+# Convert character to numeric
+dat[12:17] <- lapply(dat[12:17], function(x) {
+  if(is.factor(x)) x <- as.character(x)
+  
+  x[x == "NA"] <- NA
+  
+  as.numeric(x)
+})
+
+dat <- dat[complete.cases(dat[, 2:9]), ]
+
+dat <- plyr::rbind.fill(df, dat)
+
+## 3. Alignement trait-space between experiment-field (individual-level)
+# 3.1. Dimension reduction through a Principal Component Analysis
+PCA <- dudi.pca(dat[,c(3:10)], scannf = FALSE, nf = 7)
+
+# 3.2. Quick visualisation of eigenvalues
+screeplot(PCA, main = "Screeplot - Eigenvalues")
+
+# 3.3. Eigenvalues contribution
+(PCA$eig*100)/sum(PCA$eig)
+
+# 3.4. Quick visualisation of the PCA
+s.corcircle(PCA$co)
+
+# 3.5. Testing the number of axis to keep
+PCAtest(PCA$tab, 999, 999, 0.001, varcorr=TRUE, counter=FALSE, plot=TRUE)
+
+# 3.6. Contribution of each variable
+rowSums(100*(factoextra::get_pca_var(PCA)$cos2)[,c(1,2)])
+
+dat$Axis1 <- PCA$li$Axis1
+dat$Axis2 <- PCA$li$Axis2
+
+# Define the 5-point palette
+desat_palette <- c(
+  "#f7f7f7",  # very light gray
+  "#cccccc",  # light gray
+  "#969696",  # medium gray
+  "#636363",  # dark gray
+  "#252525"   # almost black
+)
+
+# Separation between field and experiment data
+dat <- dat %>%
+  mutate(Source = case_when(
+    !is.na(sample) ~ "field",
+    !is.na(Treatment) ~ "Experiment",
+    TRUE ~ NA_character_
+  ))
+
+PCA$co$Label <- rownames(PCA$co)
+
+# 3.7. Plot
+p <- ggplot() +
+  geom_point(data = subset(dat, Source == "field"), mapping = aes(x = Axis1, y = Axis2, color = `TN (mmol/L)`/I_m), size = 2) +
+  scale_color_gradientn(colors = desat_palette, name = "TN:Light ratio") +
   new_scale_color() +
-  geom_point(Ref_transform_average, mapping = aes(x = `Chlorophyll-a`, y = Phycocyanin, color = Treatment), size = 5) +
-  geom_abline(intercept = 0, slope = 1) +
-  stat_smooth(data = CommTraits, aes(x = `Chlorophyll-a`, y = Phycocyanin), se = FALSE, method = "lm", col = "grey20", linetype = "dashed") +
+  geom_point(data = subset(dat, Source == "Experiment"), mapping = aes(x = Axis1, y = Axis2, color = Treatment), size = 2, shape = 18) +
   scale_color_d3(name = "Treatment") +
-  theme_bw() +
-  xlim(-2.5,2.5) +
-  ylim(-2.5,2.5) +
-  xlab("Chlorophyll a fluorescence (a.u.)") +
-  ylab("Phycocyanin fluorescence (a.u.)") +
-  theme(
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 14)
-  )
-p3
-
-bottom <- plot_grid(p3, labels = c('C'))
-top_row <- plot_grid(p1, p2, labels = c('A','B'))
-p <- plot_grid(top_row, bottom, ncol =1)
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5) +
+  geom_segment(
+    data = PCA$co,
+    aes(x = 0, y = 0, xend = -Comp1*3, yend = Comp2*3),
+    col = "grey20", arrow = arrow(length = unit(0.02, "npc")),
+    inherit.aes = FALSE
+  ) +
+  geom_label_repel(
+    data = PCA$co,
+    aes(x = -Comp1*4, y = Comp2*4, label = Label),
+    col = "grey20", alpha = 0.7, size = 4, inherit.aes = FALSE
+  ) +
+  xlab("PC1 (49.0%)") +
+  ylab("PC2 (28.7%)") +
+  theme_linedraw() +
+  theme(legend.position = "left",
+        legend.text=element_text(size=12),
+        axis.text=element_text(size=18),
+        axis.title = element_text(size = 18),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()) +
+  facet_wrap(~Source)
 p
+ggsave(file="supp figure 5.svg", plot=p, width=26, height=18, units = "cm")
 
-ggsave(file="PC_Chla over time.svg", plot=p, width=20, height=20, units = "cm")
+
+## 4. Data preparation - Fig. 5
+# 4.1. Calculate barycenters (centroids) for each group
+library(dplyr)
+
+centroids <- dat %>%
+  group_by(sample, Date) %>%
+  summarise(
+    Axis1 = mean(Axis1),
+    Axis2 = mean(Axis2)
+  )
+
+centroids_treat <- dat %>%
+  group_by(Treatment) %>%
+  summarise(
+    Axis1 = mean(Axis1),
+    Axis2 = mean(Axis2)
+  )
+
+centroids_treat <- na.omit(centroids_treat)
+centroids <- merge(centroids, env[,-3], by.x = "sample", by.y = "Sample", all.x = TRUE)
+
+# Convert character to numeric
+centroids[6:11] <- lapply(centroids[6:11], function(x) {
+  if(is.factor(x)) x <- as.character(x)
+  
+  x[x == "NA"] <- NA
+  
+  as.numeric(x)
+})
+
+# Identification Nitrogen limitation in field data
+N_lim <-
+  centroids %>%
+  filter(Date == as.Date("2023-06-07") |
+           Date == as.Date("2023-06-21") |
+           Date == as.Date("2023-07-26")|
+           Date == as.Date("2023-08-09")|
+           Date == as.Date("2023-08-30")|
+           Date == as.Date("2023-09-21"))
+
+# Plot
+p <- ggplot() +
+  geom_point(data = centroids, mapping = aes(x = Axis1, y = Axis2, color = `TN (mmol/L)`/I_m), size = 4) +
+  scale_color_gradientn(colors = desat_palette, name = "TN:Light ratio") +
+  new_scale_color() +
+  geom_point(data = centroids_treat, mapping = aes(x = Axis1, y = Axis2, color = Treatment), size = 6, shape = 18) +
+  scale_color_d3(name = "Treatment") +
+  geom_vline(xintercept = 0, linetype = "dashed", linewidth = 0.5) +
+  geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.5) +
+  geom_point(data = N_lim, mapping = aes(x = Axis1, y = Axis2), col = "orange", size = 8, shape = 21,  stroke = 1.2) +
+  geom_segment(
+    data = PCA$co,
+    aes(x = 0, y = 0, xend = -Comp1*3, yend = Comp2*3),
+    col = "grey20", arrow = arrow(length = unit(0.02, "npc")),
+    inherit.aes = FALSE
+  ) +
+  geom_label_repel(
+    data = PCA$co,
+    aes(x = -Comp1*4, y = Comp2*4, label = Label),
+    col = "grey20", alpha = 0.7, size = 4, inherit.aes = FALSE
+  ) +
+  xlab("PC1 (49.0%)") +
+  ylab("PC2 (28.7%)") +
+  xlim(c(-4,4)) +
+  ylim(c(-4,4)) +
+  theme_linedraw() +
+  theme(legend.position = "left",
+        legend.text=element_text(size=12),
+        axis.text=element_text(size=18),
+        axis.title = element_text(size = 18),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+p
+ggsave(file="figure 5.svg", plot=p, width=20, height=14, units = "cm")
